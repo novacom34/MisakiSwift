b9fbdbf963601550b64ffae055fae92fea6512dd
Roma <novacom34@gmail.com>
2026-02-15 18:28:07 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   ],
   dependencies: [
     .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.30.2"),
-    .package(url: "https://github.com/novacom34/MLXUtilsLibrary", branch: "main")
+    .package(url: "https://github.com/mlalma/MLXUtilsLibrary", tag: "0.0.6")
   ],
   targets: [
     .target(
